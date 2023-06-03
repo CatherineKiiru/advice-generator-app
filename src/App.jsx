ed38a8aca3fad11918351b97778955e4fa1e5b69
Catherine <wambukukiiru@gmail.com>
2023-06-03 21:35:42 +0300

    added box shadow and centered the generate quote button

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,21 @@ import button from "./assets/images/icon-dice.svg";
 
 function App() {
   return (
-    <div className="wrapper">
-      <h3 id="advice-title">Advice #117</h3>
-      <p>
-        {`"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+    <>
+      <div className="wrapper">
+        <h3 id="advice-title">Advice #117</h3>
+        <p>
+          {`"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua."`}
-      </p>
-      <img className="divider" src={Divider} alt="" />
-      <button id="button">
-        <img className="advice-button" src={button} alt="" />
-      </button>
-    </div>
+        </p>
+        <img className="divider" src={Divider} alt="" />
+      </div>
+      <div className="button-container">
+        <button id="button">
+          <img className="advice-button" src={button} alt="" />
+        </button>
+      </div>
+    </>
   );
 }
 
