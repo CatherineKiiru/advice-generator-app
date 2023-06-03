a2563c7c9a4bf8080d091ff1b2a9de59a85977c9
Catherine <wambukukiiru@gmail.com>
2023-06-03 17:43:41 +0300

    set up project and styled the components

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import Divider from "./assets/images/pattern-divider-desktop.svg";
+import button from "./assets/images/icon-dice.svg";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <h3 id="advice-title">Advice #117</h3>
+      <p>
+        {`"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua."`}
+      </p>
+      <img className="divider" src={Divider} alt="" />
+      <button id="button">
+        <img className="advice-button" src={button} alt="" />
+      </button>
+    </div>
+  );
+}
+
+export default App;
